6ef487753d19c8bb9a6455e3b16c40cdba3dede2
Marcel Obazuaye <obazuayemarcel@gmail.com>
2026-05-12 22:35:50 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+import gdown
+import tensorflow as tf
+
+MODEL_PATH = "model.keras"
+
+# Download model if not already present
+if not os.path.exists(MODEL_PATH):
+    url = "https://drive.google.com/uc?id=1LMQYMI9hBLYfg627YDCJ9doAoZ8l-C1X"
+    gdown.download(url, MODEL_PATH, quiet=False)
+
+# Load model
+model = tf.keras.models.load_model(MODEL_PATH)
